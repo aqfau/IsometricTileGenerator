abc99dce6dca08e6f1ee015b60fabf16b6127492
AquoyaFaust <48639292+AquoyaFaust@users.noreply.github.com>
2022-02-19 20:30:19 -0600

    Woot

--- src/app/Home/Home/Home.component.ts
+++ src/app/Home/Home/Home.component.ts
@@ -8,11 +8,19 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   @ViewChild("canvassize") canvasSizeInput : ElementRef | undefined;
-  @ViewChild("canvas") canvas : ElementRef | undefined;
+  @ViewChild("canvas") canvas : ElementRef<HTMLCanvasElement> | undefined;
   @ViewChild("edgecolor") edgecolor : ElementRef | undefined;
   @ViewChild("sidefacecolor") sidefacecolor : ElementRef | undefined;
+  @ViewChild("topfacecolor") topfacecolor : ElementRef | undefined;
+  @ViewChild("preview") previewImg : ElementRef | undefined;
+  @ViewChild("filename") filenameInput : ElementRef | undefined;
 
+
+  filename : string = "New Isometric Tile"
   canvasSize : number = 100;
+  filesource : any = "";
+  canDownload : boolean = false;
+  pngSrc : any = "";
 
   constructor() { }
 
@@ -49,90 +57,180 @@ export class HomeComponent implements OnInit {
       console.log("Ctx call failed");
       return;
     }
+
+    let c = this.canvas.nativeElement as HTMLCanvasElement;
+    ctx.imageSmoothingEnabled = false;
     this.drawOutline(ctx);
-    //var img    = c.toDataURL("image/png");
-    //document.getElementById("existing-image-id").src = img;
+    var img = c.toDataURL("image/png");
+    let p = this.previewImg?.nativeElement as HTMLImageElement;
+    p.src = img;
+    this.canDownload = true;
+    this.pngSrc = p.src;
+  }
+
+  onChangeName() : void
+  {
+    this.filename = this.filenameInput?.nativeElement.value;
   }
 
   drawOutline(ctx : any)
   {
-    let region = new Path2D();
-    ctx.strokeStyle = this.edgecolor?.nativeElement.value;
-    this.drawTopOutline(ctx);
-    var fromX = 0;
-    var fromY = this.canvasSize / 4 + 1;
-    var toX = 0;
-    var toY = this.canvasSize / 4 * 3 - 1; 
-    this.drawLine(ctx, fromX, fromY, toX, toY);
-    region.lineTo(toX, toY);
-    fromX = toX;
-    fromY = toY;
-    toX = this.canvasSize / 2;
-    toY = this.canvasSize - 1
-    this.drawLine(ctx, fromX, fromY, toX, toY);
-    region.lineTo(toX, toY);
-    fromX = toX + 1;
-    fromY = toY;
-    toX = this.canvasSize;
-    toY = this.canvasSize / 4 * 3;
-    this.drawLine(ctx, fromX, fromY, toX, toY);
-    region.lineTo(toX, toY);
-    fromX = toX;
-    fromY = toY - 1;
-    toX = this.canvasSize;
-    toY = this.canvasSize / 4;
-    this.drawLine(ctx, fromX, fromY, toX, toY);
-    region.lineTo(toX, toY);
+    this.drawTop(ctx);
+    this.drawLeft(ctx);
+    this.drawRight(ctx);
   }
-
-  drawTopOutline(ctx : any) : void
+  drawLeft(ctx : any)
   {
+    let size = this.canvasSize;
     let region = new Path2D();
     ctx.strokeStyle = this.edgecolor?.nativeElement.value;
-    //stroke from topleftmiddle to topquarter
-    var fromX = this.canvasSize / 2;
-    var fromY = 0;
+
+    var fromX = 0;
+    var fromY = size / 4 - 1;
     var toX = 0;
-    var toY = this.canvasSize / 4; 
+    var toY = size / 4 * 3 - 1; 
 
     region.moveTo(fromX, fromY);
-    this.drawLine(ctx, fromX, fromY, toX, toY);
     region.lineTo(toX, toY);
 
     fromX = toX;
-    fromY = toY + 1;
-    toX = this.canvasSize / 2;
-    toY = this.canvasSize / 2;
-    
-    this.drawLine(ctx, fromX, fromY, toX, toY);
-    region.lineTo(toX, toY);
-
-    fromX = toX + 1;
     fromY = toY;
-    toX = this.canvasSize;
-    toY = this.canvasSize / 4;
+    toX = size / 2 - 1;
+    toY = size - 1;
     
-    this.drawLine(ctx, fromX, fromY, toX, toY);
     region.lineTo(toX, toY);
 
     fromX = toX;
-    fromY = toY - 1;
-    toX = this.canvasSize / 2 + 1;
-    toY = 0;
+    fromY = toY;
+    toX = size / 2 - 1;
+    toY = size / 2;
     
-    this.drawLine(ctx, fromX, fromY, toX, toY);
     region.lineTo(toX, toY);
+
+    fromX = toX;
+    fromY = toY;
+    toX = 0;
+    toY = size / 4;
+    
+    region.lineTo(toX, toY);
+    
+    region.closePath();
+    ctx.fillStyle = this.sidefacecolor?.nativeElement.value;
+    ctx.fill(region, 'nonzero');  
+    
+    ctx.strokeStyle = this.edgecolor?.nativeElement.value;
+    ctx.stroke(region, 'nonzero');
+  }
+
+  drawRight(ctx : any)
+  {
+    let size = this.canvasSize;
+    let region = new Path2D();
+
+    var fromX = size - 1;
+    var fromY = size / 4;
+    var toX = size - 1;
+    var toY = size / 4 * 3 - 1; 
+
+    region.moveTo(fromX, fromY);
+    region.lineTo(toX, toY);
+
+    fromX = toX;
+    fromY = toY;
+    toX = size / 2;
+    toY = size - 1;
+    
+    region.lineTo(toX, toY);
+
+    fromX = toX;
+    fromY = toY;
+    toX = size / 2;
+    toY = size / 2;
+    
+    region.lineTo(toX, toY);
+
+    fromX = toX;
+    fromY = toY;
+    toX = size - 1;
+    toY = size / 4;
+    
+    region.lineTo(toX, toY);
+
     region.closePath();
     ctx.fillStyle = this.sidefacecolor?.nativeElement.value;
     ctx.fill(region, 'nonzero');
+    ctx.strokeStyle = this.edgecolor?.nativeElement.value;
+    ctx.stroke(region, 'nonzero');
   }
 
-
-  drawLine(ctx : any, fromX : number, fromY : number, toX : number, toY : number) : void
+  drawTop(ctx : any) : void
   {
-    ctx.moveTo(fromX, fromY);
-    ctx.lineTo(toX, toY);
-    ctx.stroke();
+    let size = this.canvasSize;
+    let region = new Path2D();
+    ctx.strokeStyle = this.edgecolor?.nativeElement.value;
+    //stroke top-middle to left-upperquarter
+    var fromX = size / 2 - 1;
+    var fromY = 0;
+    var toX = 0;
+    var toY = size / 4 - 1; 
+
+    region.moveTo(fromX, fromY);
+    region.lineTo(toX, toY);
+
+    var fromX = toX;
+    var fromY = toY;
+    var toX = 0;
+    var toY = size / 4; 
+
+    region.lineTo(toX, toY);
+
+    fromX = toX;
+    fromY = toY;
+    toX = size / 2 - 1;
+    toY = size / 2 - 1;
+    
+    region.lineTo(toX, toY);
+
+    fromX = toX;
+    fromY = toY;
+    toX = size / 2;
+    toY = size / 2 - 1;
+    
+    region.lineTo(toX, toY);
+
+    fromX = toX;
+    fromY = toY;
+    toX = size - 1;
+    toY = size / 4;
+    
+    region.lineTo(toX, toY);
+
+    fromX = toX;
+    fromY = toY;
+    toX = size - 1;
+    toY = size / 4 - 1;
+    
+    region.lineTo(toX, toY);
+
+    fromX = toX;
+    fromY = toY;
+    toX = size / 2;
+    toY = 0;
+    
+    region.lineTo(toX, toY);
+
+    fromX = toX;
+    fromY = toY;
+    toX = size / 2 - 1;
+    toY = 0;
+    
+    region.lineTo(toX, toY);
+
+    region.closePath();
+    ctx.fillStyle = this.topfacecolor?.nativeElement.value;
+    ctx.fill(region, 'nonzero');
+    ctx.strokeStyle = this.edgecolor?.nativeElement.value;
+    ctx.stroke(region, 'nonzero');
   }
 
 }
